02c8b0f3e9402e3b6c3e4d3d598b1e865ea36f70
raja226 <iosrajasekhar@gmail.com>
2024-04-22 12:49:48 +0530

    Initial Commit

---
+++ RealmLocalDB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RealmLocalDB
+//
+//  Created by Administrator on 22/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
